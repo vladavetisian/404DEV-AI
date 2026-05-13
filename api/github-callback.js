09cac91018b55938c4b9c12bdbebd0fce157f663
vladavetisian <vladimiravetisian019@gmail.com>
2026-05-12 19:03:49 -0700

    Create github-callback.js

---
+++ api/github-callback.js
@@ -0,0 +1,33 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+  
+  if (!code) {
+    return res.status(400).send('Missing authorization code.');
+  }
+
+  try {
+    const tokenRes = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: { 
+        'Accept': 'application/json', 
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: code
+      })
+    });
+    
+    const tokenData = await tokenRes.json();
+    
+    if (tokenData.access_token) {
+      res.setHeader('Set-Cookie', `github_token=${tokenData.access_token}; Path=/; HttpOnly; SameSite=Lax; Max-Age=3600`);
+      res.redirect('/?deploy=true');
+    } else {
+      res.status(400).send('GitHub auth failed. Please try again.');
+    }
+  } catch (err) {
+    res.status(500).send('Auth server error.');
+  }
+}
