d1bc493b7cfa53a4be39222d41b8951cebcc0f72
vladavetisian <vladimiravetisian019@gmail.com>
2026-05-13 20:12:59 -0700

    Update github-oauth.js

--- api/github-oauth.js
+++ api/github-oauth.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
   const clientId = process.env.GITHUB_CLIENT_ID;
-  const host = req.headers.host;
-  const redirectUri = `https://${host}/api/github-callback`;
+  const redirectUri = 'https://404devai.vercel.app/api/github-callback';
   const scope = 'repo user';
   
   const githubUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
