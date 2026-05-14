863603678e335b99b7fda46568a6bedc6ca857fc
vladavetisian <vladimiravetisian019@gmail.com>
2026-05-14 16:52:28 -0700

    Update github-callback.js

--- api/github-callback.js
+++ api/github-callback.js
@@ -22,16 +22,10 @@ export default async function handler(req, res) {
     const tokenData = await tokenRes.json();
 
     if (tokenData.access_token) {
-      res.setHeader(
-        'Set-Cookie',
-        'github_token=' + tokenData.access_token + '; Path=/; HttpOnly; SameSite=Lax; Max-Age=3600'
-      );
-      
-      let redirectTo = '/?deploy=true';
+      var redirectTo = '/?deploy=true&token=' + tokenData.access_token;
       if (state && state.length > 0) {
-        redirectTo = '/?deploy=true&data=' + encodeURIComponent(state);
+        redirectTo += '&data=' + encodeURIComponent(state);
       }
-      
       return res.redirect(redirectTo);
     } else {
       return res.status(400).send('GitHub auth failed.');
