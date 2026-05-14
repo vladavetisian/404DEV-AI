ab30426f93b42bfb7d7245f063bd3e109aacab0c
vladavetisian <vladimiravetisian019@gmail.com>
2026-05-14 16:37:40 -0700

    Update github-oauth.js

--- api/github-oauth.js
+++ api/github-oauth.js
@@ -3,10 +3,13 @@ export default function handler(req, res) {
   const redirectUri = 'https://404dev-o0qie7n4d-404-dev-ai-s-projects.vercel.app/api/github-callback';
   const scope = 'repo user';
   
-  // Pass deploy data as state
   const state = req.query.data || '';
   
-  const githubUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&state=${encodeURIComponent(state)}`;
+  const githubUrl = 'https://github.com/login/oauth/authorize' +
+    '?client_id=' + clientId +
+    '&redirect_uri=' + encodeURIComponent(redirectUri) +
+    '&scope=' + scope +
+    '&state=' + encodeURIComponent(state);
   
   res.redirect(githubUrl);
 }
